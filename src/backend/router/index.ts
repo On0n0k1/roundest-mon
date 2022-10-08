46e9966ccaac3e1284f0061f40b0b58ef84dc9ae
On0n0k1 <stiltztinkerstein@gmail.com>
2022-10-08 10:05:38 -0300

    feat: backend will not generated random IDs any longer.
    Will return undefined types on the first render.
    
    Signed-off-by: On0n0k1 <stiltztinkerstein@gmail.com>

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -8,13 +8,29 @@ import { PokemonClient } from 'pokenode-ts';
 
 
 export const appRouter = trpc.router().query("get-pokemon-by-id", {
-  async resolve() {
-    const [first, second] = getOptionsForVote();
+  input: z.array(z.number(), z.number()),
+  async resolve(numbers) {
+    const [first, second] = numbers.input;
+
+    // These values are 0 only in the first render
+    if(first === 0 || second === 0){
+      return [
+        {
+          pokeName: undefined,
+          pokeSprite: undefined,
+        },
+        {
+          pokeName: undefined,
+          pokeSprite: undefined,
+        },
+      ];
+    }
 
     console.log('get-pokemon-id with IDs %c%d and %d',"color: yellow", first, second);
     const api = new PokemonClient();
 
     let pokemons = await Promise.all([api.getPokemonById(first), api.getPokemonById(second)]);
+    console.log("Pokemon retrieved");
 
     return pokemons.map((pokemon)=>{
       if(!pokemon.sprites.front_default){
